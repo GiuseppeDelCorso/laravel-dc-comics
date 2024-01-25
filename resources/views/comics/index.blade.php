9c7e180e3329311ce2f8f11123090a8409a9b109
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2024-01-25 17:30:41 +0100

    aggiunta css e collegamento link per navigare nella pagina

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,16 +8,22 @@
         </div>
         <div class="row">
             @foreach ($items as $element)
-                <div class="col-3">
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{ $element->thumb }}" class="card-img-top" alt="{{ $element->title }}">
+                <div class="col-4">
+                    <div class="card my-3 HeigthBodyCard" style="width: 18rem;">
+                        <img class="StyleImgCard" src="{{ $element->thumb }}" class="card-img-top" alt="{{ $element->title }}">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $element->title }}</h5>
-                            <p class="card-text">
-                                Costo -> {{ $element->price }}<br>
-                                Cosa Compro ->{{ $element->type }}
-                            </p>
-                            <a href="{{ route('comics.show', $element->id) }}" class="btn btn-primary">Mostra dettagli</a>
+                            <div class="d-flex justify-content-center">
+                                <h5 class="card-title">{{ $element->title }}</h5>
+                            </div>
+                            <div class="my-3 d-flex justify-content-center">
+                                <p class="card-text ">
+                                    Costo -> {{ $element->price }}<br>
+                                    Cosa Compro ->{{ $element->type }}
+                                </p>
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <a href="{{ route('comics.show', $element->id) }}" class="btn btn-primary">Mostra dettagli</a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,18 @@
 <header>
-    <a href="{{ route('home') }}">Home</a>
+    <div class="container">
+        <div class="row py-3 d-flex text-center">
+            <div class="col-6 ">
+                <button type="submit" class="btnHeader">
+                    <a class="colorLink" href="{{ route('comics.index') }}">Home</a>
+                </button>
+            </div>
+            <div class="col-6 ">
+                <button type="submit" class="btnHeaderAdd">
+                    <a class="colorLink" href="{{ route('comics.create') }}">Aggiungi Un Fumetto</a>
+                </button>
+            </div>
+        </div>
+    </div>
+    
+    
 </header>
